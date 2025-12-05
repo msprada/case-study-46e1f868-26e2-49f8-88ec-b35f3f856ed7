48a112cd1da2dc0caa95a1e476585e52a4f92a84
Marcel Sprada <msprada@web.de>
2025-12-05 12:21:43 +0100

    - add controller

--- room-booking-system/roombookingsystem-rest-api/src/main/java/de/vidaee/roombookingsystem/controller/UserController.java
+++ room-booking-system/roombookingsystem-rest-api/src/main/java/de/vidaee/roombookingsystem/controller/UserController.java
@@ -1,30 +1,26 @@
 package de.vidaee.roombookingsystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import de.vidaee.roombookingsystem.user.IUserRepository;
-import de.vidaee.roombookingsystem.user.User;
-import jakarta.inject.Inject;
-
+import de.vidaee.roombookingsystem.IUserRepository;
+import de.vidaee.roombookingsystem.User;
 
 @RestController
 public class UserController {
 
     private final IUserRepository userRepository;
 
-    @Inject()
+    @Autowired()
     public UserController(final IUserRepository userRepository){
         this.userRepository= userRepository;
     }
 
 
     @GetMapping("/users")
-    public List<User> list(){
-        return new ArrayList<>();
+    public Iterable<User> list(){
+        return this.userRepository.list();
     }
     
 }

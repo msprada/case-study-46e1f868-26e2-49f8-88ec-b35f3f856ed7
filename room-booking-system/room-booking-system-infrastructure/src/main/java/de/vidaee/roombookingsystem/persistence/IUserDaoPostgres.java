c347e1bcc9fae1185d0dbf7eca74cf0295be93e1
Marcel Sprada <msprada@web.de>
2026-01-04 11:37:02 +0100

    - implement jpa Repository

--- room-booking-system/room-booking-system-infrastructure/src/main/java/de/vidaee/roombookingsystem/persistence/IUserDaoPostgres.java
+++ room-booking-system/room-booking-system-infrastructure/src/main/java/de/vidaee/roombookingsystem/persistence/IUserDaoPostgres.java
@@ -1,9 +1,8 @@
 package de.vidaee.roombookingsystem.persistence;
 
-// import de.vidaee.roombookingsystem.entitities.User;
-import de.vidaee.roombookingsystem.persistence.entities.UserDao;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import de.vidaee.roombookingsystem.persistence.entities.UserDao;
 
 public interface IUserDaoPostgres extends JpaRepository<UserDao, Long>  {
     
